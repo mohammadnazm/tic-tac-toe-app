492485636a9ace86141becd1e778baf57a961529
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-02 11:34:32 +0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
   return <div className="App"></div>
 }
 
