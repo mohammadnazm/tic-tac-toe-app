3c1b5f2283e13a3340cb4ad18f37090e246030a1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-02 14:07:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,8 +46,46 @@ function App() {
             }}
           />
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3)
+            }}
+          />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4)
+            }}
+          />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5)
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6)
+            }}
+          />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7)
+            }}
+          />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8)
+            }}
+          />
+        </div>
       </div>
     </div>
   )
